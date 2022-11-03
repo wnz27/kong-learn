a269c63bf39d7d3b69f1b575829fc22af31c03fd
chronolaw <chrono_cpp@me.com>
2022-11-03 14:11:57 +0800

    rename to init_register

--- kong/clustering/init.lua
+++ kong/clustering/init.lua
@@ -67,7 +67,7 @@ end
 
 function _M:init_cp_worker(plugins_list)
 
-  events.init_register_events()
+  events.init_register()
 
   self.json_handler:init_worker(plugins_list)
   if not kong.configuration.legacy_hybrid_protocol then
